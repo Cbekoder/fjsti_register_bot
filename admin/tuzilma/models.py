c18ad9f54e3008f0559d042d4ab15f13c9caf7ca
cbekoder <cbekoder@gmail.com>
2025-05-27 12:07:43 +0500

    fix

--- admin/tuzilma/models.py
+++ admin/tuzilma/models.py
@@ -10,66 +10,6 @@ LEVEL_NAME_CHOICES = (
     ("Ordinatura", "Ordinatura"),
 )
 
-DIRECTION_NAME_CHOICES = (
-    ("Biotibbiyot muhandisligi", "Biotibbiyot muhandisligi"),
-    ("Oliy hamshiralik ishi", "Oliy hamshiralik ishi"),
-    ("Tibbiy profilaktika ishi", "Tibbiy profilaktika ishi"),
-    ("Davolash ishi", "Davolash ishi"),
-    ("Farmatsiya", "Farmatsiya"),
-    ("Pediatriya ishi", "Pediatriya ishi"),
-    ("Stomatologiya", "Stomatologiya"),
-    ("Fundamental tibbiyot", "Fundamental tibbiyot"),
-    ("Xalq tabobati", "Xalq tabobati"),
-    ("Akusherlik", "Akusherlik"),
-    ("Endokrinologiya", "Endokrinologiya"),
-    ("Gigiena", "Gigiena"),
-    ("Kardiologiya", "Kardiologiya"),
-    ("Laboratoria ishi", "Laboratoria ishi"),
-    ("Morfologiya", "Morfologiya"),
-    ("Otorinolaringologiya", "Otorinolaringologiya"),
-    ("Patologik anatomiya", "Patologik anatomiya"),
-    ("Rentgent texnikasi va texnologiyasi", "Rentgent texnikasi va texnologiyasi"),
-    ("Sog‘liqni saqlashni boshqarish va jamoat sog‘lig‘ini saqlash", "Sog‘liqni saqlashni boshqarish va jamoat sog‘lig‘ini saqlash"),
-    ("Terapiya", "Terapiya"),
-    ("Tibbiy-biologik apparatlar, tizimlar va majmualar", "Tibbiy-biologik apparatlar, tizimlar va majmualar"),
-    ("Umumiy onkologiya", "Umumiy onkologiya"),
-    ("Urologiya", "Urologiya"),
-    ("Xirurgiya", "Xirurgiya"),
-    ("Akusherlik va ginekologiya", "Akusherlik va ginekologiya"),
-    ("Allergologiya va klinik immunologiya", "Allergologiya va klinik immunologiya"),
-    ("Anesteziologiya va reanimatologiya", "Anesteziologiya va reanimatologiya"),
-    ("Bolalar anesteziologiya va reanimatologiyasi", "Bolalar anesteziologiya va reanimatologiyasi"),
-    ("Bolalar kardiorevmatalogiyasi", "Bolalar kardiorevmatalogiyasi"),
-    ("Bolalar nefrologiyasi", "Bolalar nefrologiyasi"),
-    ("Bolalar nevrologiyasi", "Bolalar nevrologiyasi"),
-    ("Bolalar va o‘smir qizlar ginekologiyasi", "Bolalar va o‘smir qizlar ginekologiyasi"),
-    ("Bolalar xirurgiyasi", "Bolalar xirurgiyasi"),
-    ("Dermatovenerologiya", "Dermatovenerologiya"),
-    ("Epidemiologiya", "Epidemiologiya"),
-    ("Ftiziatriya", "Ftiziatriya"),
-    ("Gemotologiya va transfuziologiya", "Gemotologiya va transfuziologiya"),
-    ("Ichki kasalliklar terapiya", "Ichki kasalliklar terapiya"),
-    ("Kommunal gigiena", "Kommunal gigiena"),
-    ("Mehnat gigienasi", "Mehnat gigienasi"),
-    ("Narkologiya", "Narkologiya"),
-    ("Nefrologiya", "Nefrologiya"),
-    ("Nefrologiya gtmodializ bilan", "Nefrologiya gtmodializ bilan"),
-    ("Neonatologiya", "Neonatologiya"),
-    ("Nevrologiya", "Nevrologiya"),
-    ("Neyroxirurgiya", "Neyroxirurgiya"),
-    ("Nutritsiologiya", "Nutritsiologiya"),
-    ("Oftalmologiya", "Oftalmologiya"),
-    ("Psixiatriya", "Psixiatriya"),
-    ("Pulmonologiya", "Pulmonologiya"),
-    ("Revmatologiya", "Revmatologiya"),
-    ("Terapevtik stomatologiya", "Terapevtik stomatologiya"),
-    ("Tibbiy psixologiya", "Tibbiy psixologiya"),
-    ("Tibbiy radiologiya", "Tibbiy radiologiya"),
-    ("Travmatologiya va ortopediya", "Travmatologiya va ortopediya"),
-    ("Yuqumli kasalliklar", "Yuqumli kasalliklar"),
-    ("Yuz jag‘ jarroxligi", "Yuz jag‘ jarroxligi"),
-)
-
 class Level(models.Model):
     name = models.CharField(max_length=100, choices=LEVEL_NAME_CHOICES, unique=True)
     is_active = models.BooleanField(default=True)
@@ -82,7 +22,7 @@ class Level(models.Model):
         return self.name
 
 class Direction(models.Model):
-    name = models.CharField(max_length=100, choices=DIRECTION_NAME_CHOICES, unique=True)
+    name = models.CharField(max_length=100)
     level = models.ForeignKey(Level, on_delete=models.SET_NULL, null=True, blank=False)
     is_active = models.BooleanField(default=True)
 
